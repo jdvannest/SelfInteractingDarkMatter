216236f975fad817179e79377ca97e1f85c14dee
jdvannest (macbook) <jordanvannest@gmail.com>
2021-10-05 15:45:03 -0500

    Added contour plots to DarkMatterShape Plots

--- Code/DarkMatterShapes.Plots.py
+++ Code/DarkMatterShapes.Plots.py
@@ -1,13 +1,22 @@
 import argparse,pickle
+import numpy as np
 import matplotlib.pylab as plt
 from osxmetadata import OSXMetaData
+from scipy.stats import kde
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-c","--cross_section",required=True,choices=['3','10','30','50'])
+parser.add_argument("-c","--cross_section",required=True,choices=['cdm','3','10','30','50'])
 #parser.add_argument("-p","--pynbody",action="store_true")
 args = parser.parse_args()
 
-data = pickle.load(open(f'../DataFiles/DarkMatterShapes.SI{args.cross_section}.pickle','rb'))
+if args.cross_section == 'cdm':
+    filename = f'../DataFiles/DarkMatterShapes.CDM.pickle'
+    im = 'CDM'
+else:
+    filename = f'../DataFiles/DarkMatterShapes.SI{args.cross_section}.pickle'
+    im = f'SI{args.cross_section}'
+
+data = pickle.load(open(filename,'rb'))
 
 b,c = [[],[]]
 for halo in data:
@@ -19,8 +28,29 @@ ax.set_xlim([0,1])
 ax.set_ylim([0,1])
 ax.set_xlabel('b/a',fontsize=25)
 ax.set_ylabel('c/a',fontsize=25)
+ax.set_title(im,fontsize=25)
 ax.plot([0,1],[0,1],c='0.5',linestyle='--')
 ax.scatter(b,c,c='k',marker='.',s=.75**2)
-f.savefig(f'../Plots/DarkMatterShapes.SI{args.cross_section}.AHF.png',bbox_inches='tight',pad_inches=.1)
-meta = OSXMetaData(f'../Plots/DarkMatterShapes.SI{args.cross_section}.AHF.png')
+f.savefig(f'../Plots/DarkMatterShapes.{im}.AHF.png',bbox_inches='tight',pad_inches=.1)
+meta = OSXMetaData(f'../Plots/DarkMatterShapes.{im}.AHF.png')
+meta.creator='DarkMatterShapes.Plots.py'
+
+
+nbins=100
+bins = np.linspace(0,1,101)
+k = kde.gaussian_kde([b,c])
+xk, yk = np.mgrid[min(b):max(b):nbins*1j, min(c):max(c):nbins*1j]
+zk = k(np.vstack([xk.flatten(), yk.flatten()]))
+
+f,ax = plt.subplots(1,1,figsize=(8,8))
+ax.set_xlim([0,1])
+ax.set_ylim([0,1])
+ax.set_xlabel('b/a',fontsize=25)
+ax.set_ylabel('c/a',fontsize=25)
+ax.set_title(im,fontsize=25)
+ax.plot([0,1],[0,1],c='0.5',linestyle='--')
+ax.scatter(b,c,c='k',marker='.',s=.75**2)
+ax.contour(xk, yk, zk.reshape(xk.shape),levels=3,cmap='spring')
+f.savefig(f'../Plots/DarkMatterShapes.{im}.AHF.Contour.png',bbox_inches='tight',pad_inches=.1)
+meta = OSXMetaData(f'../Plots/DarkMatterShapes.{im}.AHF.Contour.png')
 meta.creator='DarkMatterShapes.Plots.py'
