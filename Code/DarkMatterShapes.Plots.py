cd7030dfae0ca14d6928cb04ccde026e51d85dee
jdvannest (macbook) <jordanvannest@gmail.com>
2021-10-05 19:59:35 -0500

    Added Dark Matter Shape Plots for pynbody data

--- Code/DarkMatterShapes.Plots.py
+++ Code/DarkMatterShapes.Plots.py
@@ -6,7 +6,7 @@ from scipy.stats import kde
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c","--cross_section",required=True,choices=['cdm','3','10','30','50'])
-#parser.add_argument("-p","--pynbody",action="store_true")
+parser.add_argument("-p","--pynbody",action="store_true")
 args = parser.parse_args()
 
 if args.cross_section == 'cdm':
@@ -18,10 +18,12 @@ else:
 
 data = pickle.load(open(filename,'rb'))
 
-b,c = [[],[]]
+b,c,bpyn,cpyn = [[],[],[],[]]
 for halo in data:
     b.append(data[halo]['b'])
     c.append(data[halo]['c'])
+    bpyn.append(data[halo]['b_pyn'][-1])
+    cpyn.append(data[halo]['c_pyn'][-1])
 
 f,ax = plt.subplots(1,1,figsize=(8,8))
 ax.set_xlim([0,1])
@@ -35,7 +37,6 @@ ax.scatter(b,c,c='k',marker='.',s=.75**2)
 f.savefig(f'../Plots/DarkMatterShapes.{im}.AHF.png',bbox_inches='tight',pad_inches=.1)
 meta = OSXMetaData(f'../Plots/DarkMatterShapes.{im}.AHF.png')
 meta.creator='DarkMatterShapes.Plots.py'
-
 
 nbins=100
 bins = np.linspace(0,1,101)
@@ -78,3 +79,59 @@ ax3.hist(c,bins,histtype='step',facecolor='None',edgecolor='k',density=True,orie
 f.savefig(f'../Plots/DarkMatterShapes.{im}.AHF.Histogram.png',bbox_inches='tight',pad_inches=.1)
 meta = OSXMetaData(f'../Plots/DarkMatterShapes.{im}.AHF.Histogram.png')
 meta.creator='DarkMatterShapes.Plots.py'
+
+if args.pynbody:
+    f,ax = plt.subplots(1,1,figsize=(8,8))
+    ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+    ax.tick_params(length=5,labelsize=15)
+    ax.set_xlabel('b/a',fontsize=25)
+    ax.set_ylabel('c/a',fontsize=25)
+    ax.set_title(im,fontsize=25)
+    ax.plot([0,1],[0,1],c='0.5',linestyle='--')
+    ax.scatter(bpyn,cpyn,c='k',marker='.',s=.75**2)
+    f.savefig(f'../Plots/DarkMatterShapes.{im}.Pynbody.png',bbox_inches='tight',pad_inches=.1)
+    meta = OSXMetaData(f'../Plots/DarkMatterShapes.{im}.Pynbody.png')
+    meta.creator='DarkMatterShapes.Plots.py'
+
+    nbins=100
+    bins = np.linspace(0,1,101)
+    k = kde.gaussian_kde([bpyn,cpyn])
+    xk, yk = np.mgrid[min(bpyn):max(bpyn):nbins*1j, min(cpyn):max(cpyn):nbins*1j]
+    zk = k(np.vstack([xk.flatten(), yk.flatten()]))
+
+    f,ax = plt.subplots(1,1,figsize=(8,8))
+    ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+    ax.tick_params(length=5,labelsize=15)
+    ax.set_xlabel('b/a',fontsize=25)
+    ax.set_ylabel('c/a',fontsize=25)
+    ax.set_title(im,fontsize=25)
+    ax.plot([0,1],[0,1],c='0.5',linestyle='--')
+    ax.scatter(bpyn,cpyn,c='k',marker='.',s=.75**2)
+    ax.contour(xk,yk,zk.reshape(xk.shape),levels=3,cmap='spring')
+    f.savefig(f'../Plots/DarkMatterShapes.{im}.Pynbody.Contour.png',bbox_inches='tight',pad_inches=.1)
+    meta = OSXMetaData(f'../Plots/DarkMatterShapes.{im}.Pynbody.Contour.png')
+    meta.creator='DarkMatterShapes.Plots.py'
+
+
+    f,ax = plt.subplots(1,1,figsize=(8,8))
+    ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+    ax.tick_params(length=5,labelsize=15)
+    ax.set_xlabel('b/a',fontsize=25)
+    ax.set_ylabel('c/a',fontsize=25)
+    ax.set_title(im,fontsize=25)
+    ax.plot([0,1],[0,1],c='0.5',linestyle='--')
+    ax.hist2d(bpyn,cpyn,[bins,bins],cmap='Greys',density=True)
+    ax2 = ax.twinx()
+    ax2.set_ylim([0,50])
+    ax2.set_yticks([])
+    ax2.hist(bpyn,bins,histtype='step',facecolor='None',edgecolor='k',density=True)
+    ax3 = ax.twiny()
+    ax3.set_xlim([0,50])
+    ax3.set_xticks([])
+    ax3.hist(cpyn,bins,histtype='step',facecolor='None',edgecolor='k',density=True,orientation='horizontal')
+    f.savefig(f'../Plots/DarkMatterShapes.{im}.Pynbody.Histogram.png',bbox_inches='tight',pad_inches=.1)
+    meta = OSXMetaData(f'../Plots/DarkMatterShapes.{im}.Pynbody.Histogram.png')
+    meta.creator='DarkMatterShapes.Plots.py'
