2d25aea9d884cc4a6688270c85806bc85c9ee814
jdvannest (macbook) <jordanvannest@gmail.com>
2022-05-26 15:13:14 -0500

    Added config file to update paths per machine and allow for different redshifts

--- Code/AHFData.py
+++ Code/AHFData.py
@@ -1,4 +1,4 @@
-import argparse,sys
+import argparse,os,pickle,sys
 import numpy as np
 def myprint(string,clear=False):
     if clear:
@@ -8,42 +8,35 @@ def myprint(string,clear=False):
 
 parser = argparse.ArgumentParser(description='', usage='')
 parser.add_argument('-c','--cross_section',choices=['CDM','SI3','SI10','vdXsec'],required=True)
-parser.add_argument('-n','--npart',type=int,default=300)
+parser.add_argument('-z','--redshift',choices=['z0','z1','z2','z3','z4'],required=True)
+parser.add_argument('-n','--npart',type=int,default=64)
 args = parser.parse_args()
 
-simpaths = {
-    'CDM':'/home/vannest/dwarf_volumes/storm.SIDM/storm.cosmo25cmb.4096/storm.cosmo25cmb.4096.004096',
-    'SI3':'/home/vannest/dwarf_volumes/storm.SIDM/storm.cosmo25cmbSI3.4096/storm.cosmo25cmbSI3.4096.004096',
-    'SI10':'/home/vannest/dwarf_volumes/storm.SIDM/storm.cosmo25cmbSI10.4096/storm.cosmo25cmbSI10.4096.004096',
-    'vdXsec':'/home/vannest/dwarf_volumes/storm.SIDM/storm.cosmo25cmbvdXsec.65536/storm.cosmo25cmbvdXsec.65536.065536'
-}
-AHFs = {
-    'CDM':'/home/vannest/dwarf_volumes/storm.SIDM/storm.cosmo25cmb.4096/storm.cosmo25cmb.4096.004096.0000.z0.000.AHF_halos',
-    'SI3':'/home/vannest/dwarf_volumes/storm.SIDM/storm.cosmo25cmbSI3.4096/storm.cosmo25cmbSI3.4096.004096.0000.z0.000.AHF_halos',
-    'SI10':'/home/vannest/dwarf_volumes/storm.SIDM/storm.cosmo25cmbSI10.4096/storm.cosmo25cmbSI10.4096.004096.0000.z0.000.AHF_halos',
-    'vdXsec':'/home/vannest/dwarf_volumes/storm.SIDM/storm.cosmo25cmbvdXsec.65536/storm.cosmo25cmbvdXsec.65536.065536.z0.000.AHF_halos'
-}
+#Update paths depending on machine
+os.system(f'python Config.py')
+config = pickle.load(open('Config.pickle','rb'))
+AHFs = config[{args.redshift}]['AHFs']
 
 print('Finding Halos')
 Nhalo = 0
 with open(AHFs[args.cross_section]) as f:
     AHF = f.readlines()
     del AHF[0]
-index = 4 if args.cross_section=='vdXsec' else 3
+index = 4 
 for line in AHF:
     if int(line.split('\t')[index])>(args.npart-1): Nhalo+=1
 myprint(f'{Nhalo} Halos Found.',clear=True)
 
 Mvir,Rvir,cont = np.zeros(Nhalo),np.zeros(Nhalo),np.zeros(Nhalo)
-m_index = 3 if args.cross_section=='vdXsec' else 2
-r_index = 11 if args.cross_section=='vdXsec' else 10
-c_index = 37 if args.cross_section=='vdXsec' else 26
+m_index = 3
+r_index = 11
+c_index = 37
 for i in np.arange(Nhalo):
     Mvir[i] = float(AHF[i].split('\t')[m_index])
     Rvir[i] = float(AHF[i].split('\t')[r_index])
     cont[i] = float(AHF[i].split('\t')[c_index])
 
-np.save(f'../DataFiles/Mvir.N{args.npart}.{args.cross_section}.z0.npy',Mvir)
-np.save(f'../DataFiles/Rvir.N{args.npart}.{args.cross_section}.z0.npy',Rvir)
-np.save(f'../DataFiles/ContaminationFraction.N{args.npart}.{args.cross_section}.z0.npy',cont)
+np.save(f'../DataFiles/Mvir.N{args.npart}.{args.cross_section}.{args.redshift}.npy',Mvir)
+np.save(f'../DataFiles/Rvir.N{args.npart}.{args.cross_section}.{args.redshift}.npy',Rvir)
+np.save(f'../DataFiles/ContaminationFraction.N{args.npart}.{args.cross_section}.{args.redshift}.npy',cont)
 print('Done')
